135f75c783065460811f0694ec4a5a6443509dd7
Ben Schmeckpeper <ben@aisle50.com>
2013-03-27 10:20:44 -0500

    Preliminary parsing of an input file.

--- score.c
+++ score.c
@@ -7,6 +7,7 @@
 struct _team; typedef struct _team TEAM;
 typedef char SEED;
 
+int parse(FILE *, TEAM **);
 int process(TEAM *);
 int score_outcome(SEED *, TEAM *);
 
@@ -19,6 +20,46 @@ struct _team
 	SEED region_lr[15];
 	SEED final_four[3];
 };
+
+/*
+ * Given an open file stream, parse the input and create the corresponding team
+ * data structures.
+ */
+int
+parse(fp, teampp)
+     FILE *fp;
+     TEAM **teampp;
+{
+  TEAM *teamp;
+  char *c, *name, *row;
+  int i, len, nteams, offset;
+
+  offset = -1;
+  nteams = 10;
+  teamp = (TEAM *)malloc(nteams * sizeof(TEAM));
+  row = (char *)malloc(LINESZ * sizeof(char));
+
+  while (fgets(row, LINESZ, fp) != NULL) {
+    c = strchr(row, ',');
+    len = c - row;
+    name = (char *)malloc((len + 1) * sizeof(char));
+    strncpy(name, row, len);
+    name[len] = '\0';
+    if (offset < 0 || strncmp(name, teamp[offset].name, len + 1) != 0) {
+      offset += 1;
+      if (offset >= nteams) {
+        nteams *= 2;
+        teamp = realloc(teamp, nteams * sizeof(TEAM));
+      }
+      teamp[offset].name = name;
+    }
+  }
+
+  for (i = 0; i <= offset; i++)
+    printf("TEAM: %s\n", teamp[i].name);
+  
+  return 0;
+}
 
 int process(teams)
 	TEAM *teams;
@@ -41,7 +82,15 @@ int process(teams)
     return 0;
 }
 
-void main() {
-    process(NULL);
+int main(argc, argv)
+     char *argv;
+     int argc;
+{
+  FILE *fp;
+  TEAM *teamp;
+  int ret;
+
+  fp = fopen("picks", "r");
+  ret = parse(fp, &teamp);
 }
 
