2580f0a0f0ca3b8caf85e5dce3acc862d2b8e672
Ben Schmeckpeper <ben@aisle50.com>
2013-03-28 00:13:34 -0500

    Change parsing now that all picks are on one line.

--- score.c
+++ score.c
@@ -8,18 +8,14 @@ struct _team; typedef struct _team TEAM;
 typedef char SEED;
 
 int parse(FILE *, TEAM **);
-int process(TEAM *, SEED *, int, int);
-int score_outcome(TEAM *, SEED *);
+int process(TEAM *, int, SEED *, int, int);
+int score_outcome(TEAM *, int, SEED *);
 int score_team(TEAM *, SEED *);
 
 struct _team
 {
 	char *name;
-	SEED region_ul[14];
-	SEED region_ll[14];
-	SEED region_ur[14];
-	SEED region_lr[14];
-	SEED final_four[7];
+    SEED prediction[63];
 };
 
 /*
@@ -32,16 +28,22 @@ parse(fp, teampp)
      TEAM **teampp;
 {
   SEED *seedp;
-  TEAM *teamp;
+  TEAM *teams;
   char *chunk, *comma, *name, *row;
-  int end_of_line, final_four, i, j, multiplier, nteams, offset;
+  int end_of_line, final_four, i, j, multiplier, nteams, offset, pick;
 
-  offset = -1;
+  i = -1;
   nteams = 10;
-  teamp = (TEAM *)malloc(nteams * sizeof(TEAM));
+  teams = (TEAM *)malloc(nteams * sizeof(TEAM));
   row = (char *)malloc(LINESZ * sizeof(char));
 
   while (fgets(row, LINESZ, fp) != NULL) {
+    i++;
+    if (i >= nteams) {
+      nteams *= 2;
+      teams = realloc(teams, nteams * sizeof(TEAM));
+    }
+
     chunk = row;
     
     /* The row starts with a name. */
@@ -49,41 +51,10 @@ parse(fp, teampp)
     if (comma == NULL)
       return -1;
     *comma = '\0';
-    name = (char *)malloc((strlen(chunk) + 1) * sizeof(char));
-    strcpy(name, chunk);
-    /* If this is a new name, or the first name, set the team's name. */
-    if (offset < 0 || strcmp(name, teamp[offset].name) != 0) {
-      offset++;
-      /* Dynamically grow the team array, if needed. */
-      if (offset >= nteams) {
-        nteams *= 2;
-        teamp = realloc(teamp, nteams * sizeof(TEAM));
-      }
-      teamp[offset].name = name;
-    }
-  
-    /* Next comes the region identifier. */
-    chunk = comma + 1;
-    comma = strchr(chunk, ',');
-    *comma = '\0';
-    if (strncmp(chunk, "UL", 2) == 0) {
-      seedp = teamp[offset].region_ul;
-      multiplier = 0;
-    } else if (strncmp(chunk, "LL", 2) == 0) {
-      seedp = teamp[offset].region_ll;
-      multiplier = 1;
-    } else if (strncmp(chunk, "UR", 2) == 0) {
-      seedp = teamp[offset].region_ur;
-      multiplier = 2;
-    } else if (strncmp(chunk, "LR", 2) == 0) {
-      seedp = teamp[offset].region_lr;
-      multiplier = 3;
-    } else if (strncmp(chunk, "FF", 2) == 0) {
-      seedp = teamp[offset].final_four;
-    } else
-      printf("Unknown region: %s\n", chunk);
-
-    final_four = seedp == teamp[offset].final_four;
+    teams[i].name = (char *)malloc((strlen(chunk) + 1) * sizeof(char));
+    strcpy(teams[i].name, chunk);
+    
+    seedp = teams[i].prediction;
     end_of_line = 0;
     while (!end_of_line) {
       chunk = comma + 1;
@@ -95,28 +66,24 @@ parse(fp, teampp)
         end_of_line = 1;
       }
       *comma = '\0';
-      /* The final four seeds are prefixed with the team's region. */
-      if (final_four) {
-        if (strncmp(chunk, "UL", 2) == 0)
-          multiplier = 0;
-        else if (strncmp(chunk, "LL", 2) == 0)
-          multiplier = 1;
-        else if (strncmp(chunk, "UR", 2) == 0)
-          multiplier = 2;
-        else if (strncmp(chunk, "LR", 2) == 0)
-          multiplier = 3;
-        else
-          printf("Unknown region: %s\n", chunk);
-        chunk += 2;
-      }
-      *seedp++ = (SEED)atoi(chunk) + 16*multiplier;
+          
+      *seedp++ = (SEED)atoi(chunk);
     }
+  }
+  nteams = i + 1;
+  
+  for (i = 0; i < nteams; i++) {
+    printf("%s", teams[i].name);
+    for (j = 0; j < 63; j++) {
+      printf(",%d", teams[i].prediction[j]);
+    }
+    printf("\n");
   }
 
   return 0;
 }
 
-int process(teams, gamestate, matchup, index)
+int process(teams, nteams, gamestate, matchup, index)
 	TEAM *teams;
     SEED *gamestate;
     int index, matchup;
@@ -124,21 +91,22 @@ int process(teams, gamestate, matchup, index)
   int offset, ret;
   
   if (index > 126)
-    return score_outcome(teams, gamestate);
+    return score_outcome(teams, nteams, gamestate);
 
   gamestate[index] = gamestate[matchup];
-  if ((ret = process(teams, gamestate, matchup + 2, index + 1)) != 0)
+  if ((ret = process(teams, nteams, gamestate, matchup + 2, index + 1)) != 0)
     return ret;
   gamestate[index] = gamestate[matchup + 1];
-  if ((ret = process(teams, gamestate, matchup + 2, index + 1)) != 0)
+  if ((ret = process(teams, nteams, gamestate, matchup + 2, index + 1)) != 0)
     return ret;
   return 0;
 }
 
 int
-score_outcome(teams, outcome)
+score_outcome(teams, nteams, outcome)
      TEAM *teams;
      SEED *outcome;
+     int nteams;
 {
   TEAM *team;
   int hi_score, i, nwinners, score, *tids, tid_size;
@@ -148,8 +116,8 @@ score_outcome(teams, outcome)
   nwinners = 0;
   hi_score = 0;
 
-  for (i = 0; teams[i] != NULL; i++) {
-    score = score_team(teams[i], outcome);
+  for (i = 0; i < nteams; i++) {
+    score = score_team(&teams[i], outcome);
     if (score > hi_score) {
       tids[0] = i;
       nwinners = 1;
@@ -157,7 +125,7 @@ score_outcome(teams, outcome)
       tids[nwinners++] = i;
       if (nwinners > tid_size) {
         tid_size *= 2;
-        tids = (int *)realloc(tid_size * sizeof(int));
+        tids = (int *)realloc(tids, tid_size * sizeof(int));
       }
     }
   }
